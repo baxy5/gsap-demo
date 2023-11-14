f40ac97635b7a3e7d595d8f137c9c6fb46ece522
baxy5 <bakos.soda@gmail.com>
2023-11-14 17:51:46 +0100

    ddd

--- components/Wow.vue
+++ components/Wow.vue
@@ -2,12 +2,38 @@
   <main>
     <div
       id="container"
-      class="relative h-[100vh] w-full flex justify-center items-center"
+      class="relative bg-white h-[100vh] w-full flex justify-center items-center"
     >
-    <p class="text-anim text-white text-8xl">We love to wow</p>
-    <img class="text-anim absolute top-0 right-0" src="../assets/landing-top.png" />
-    <img class="text-anim absolute top-0 left-0" src="../assets/landing-top-left.png" />
-    <img class="text-anim absolute bottom-0 right-[50%]" src="../assets/landing-bottom.png" />
+      <p
+        id="text1"
+        class="absolute top-[40%] left-[20%] text-white text-[4rem] z-50"
+      >
+        We love to wow
+      </p>
+      <p class="text-black text-[6rem] appic z-50 mt-96">Appic</p>
+      <svg
+        class="svg absolute top-0 h-full w-full z-40"
+        width="1794"
+        height="1219"
+        viewBox="0 0 1794 1219"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <defs>
+          <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" style="stop-color: rgba(53, 48, 139, 1)" />
+            <stop offset="35%" style="stop-color: rgba(96, 96, 213, 1)" />
+            <stop offset="100%" style="stop-color: rgba(0, 212, 255, 1)" />
+          </linearGradient>
+        </defs>
+        <path
+          id="path"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M1203.94 43.3272C1203.94 19.3983 1184.54 0 1160.61 0H705.23C681.587 0 658.97 9.66102 642.623 26.7432L16.0239 681.537C5.73962 692.284 -0.00048825 706.586 3.11491e-08 721.461L0.0140398 1175.02C0.0147805 1198.95 19.4128 1218.35 43.3413 1218.35H498.734C522.366 1218.35 544.972 1208.69 561.311 1191.62L1033.94 697.711C1060.95 669.489 1108.58 688.606 1108.58 727.668L1108.57 1005.66C1108.57 1027.57 1116.87 1048.67 1131.81 1064.71L1257.56 1199.66C1268.49 1211.38 1283.79 1218.04 1299.81 1218.04H1749.74C1773.66 1218.04 1793.06 1198.64 1793.06 1174.71L1793.08 726.39C1793.08 708.127 1786.15 690.542 1773.7 677.181L1648.36 542.676C1634.7 528.018 1615.57 519.694 1595.53 519.694H1247.27C1223.34 519.694 1203.94 500.296 1203.94 476.367V43.3272Z"
+          fill="#001323"
+        />
+      </svg>
     </div>
   </main>
 </template>
@@ -23,20 +49,35 @@ export default {
   mounted() {
     let tl = gsap.timeline({
       scrollTrigger: {
-        trigger: '.text-anim',
+        trigger: '.svg',
         scrub: 1,
         pin: '#container',
         start: 'center center',
       },
     })
 
-    tl.from('.text-anim', {
-      scale: 30,
+    tl.set(['.appic', '#text1', '.svg'], {
       opacity: 0,
-    }).to('.text-anim', {
-      scale: 0,
-      opacity: 0,
+      display: 'hidden',
     })
+      .from('.svg', {
+        scale: 8,
+        display: 'block',
+      })
+      .to('.svg', {
+        scale: 0.3,
+      })
+      .to(
+        '#path',
+        {
+          fill: 'url(#gradient)',
+        },
+        '<'
+      )
+      .to('.appic', {
+        opacity: 4,
+        display: 'block',
+      })
   },
 }
 </script>
--- pages/panel.vue
+++ pages/panel.vue
@@ -1,13 +1,36 @@
 <template>
   <main>
-    <div class="panel1 h-[100vh] bg-red-500">
-      <div class="text">
-        <h1>asd</h1>
-        <p>asdasdsad</p>
+    <div class="h-[40vh]"></div>
+    <section class="bg-[#001234] pb-[200px]">
+      <div class="grid grid-cols-2 h-full w-full">
+        <div class="w-full h-full main-container">
+          <div class="box1 h-[100vh] flex justify-center items-center">
+            <p class="text1 opacity-[0] translate-y-[75px] text-white">
+              Predicting delivery with an advanced analytics model, straight
+              from your pocket.
+            </p>
+          </div>
+          <div class="box2 h-[100vh] flex justify-center items-center">
+            <p class="text2 opacity-[0] translate-y-[75px] text-white">
+              Predicting delivery with an advanced analytics model, straight
+              from your pocket.
+            </p>
+          </div>
+          <div class="box3 h-[100vh] flex justify-center items-center">
+            <p class="text3 opacity-[0] translate-y-[75px] text-white">
+              Predicting delivery with an advanced analytics model, straight
+              from your pocket.
+            </p>
+          </div>
+        </div>
+        <div
+          class="fixed-container w-full h-[100vh] flex justify-center items-center"
+        >
+          <img class="w-[550px] h-auto" src="../assets/ps.jpg" />
+        </div>
       </div>
-    </div>
-    <div class="panel2 h-[700px] bg-orange-500"><p>asd</p></div>
-    <div class="panel3 h-[850px] bg-green-500"><p>asd</p></div>
+    </section>
+    <div class="h-[30vh]"></div>
   </main>
 </template>
 <script>
@@ -19,12 +42,56 @@ if (typeof window !== 'undefined') {
 }
 export default {
   mounted() {
-    ScrollTrigger.create({
-      trigger: '.panel2',
-      start: 'top top',
-      pin: '.panel2',
-      pinSpacing: false,
-      markers: true,
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.main-container',
+        pin: '.fixed-container',
+        scrub: 2,
+        start: 'top top',
+        end: 'bottom bottom',
+      },
+    })
+
+    let box1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.box1',
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: 2,
+      },
+    })
+
+    box1.to('.text1', {
+      opacity: 4,
+      y: 0,
+    })
+
+    let box2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.box2',
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: 2,
+      },
+    })
+
+    box2.to('.text2', {
+      opacity: 4,
+      y: 0,
+    })
+
+    let box3 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.box3',
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: 2,
+      },
+    })
+
+    box3.to('.text3', {
+      opacity: 4,
+      y: 0,
     })
   },
 }
